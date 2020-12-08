01fb171a1157a8ad1df2e332ac124852d42a36bd
Sergey Tkachenko <tkachenkosj@gmail.com>
2020-12-08 15:01:03 +0300

    Initial Commit

---
+++ CW9 - Animation test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CW9 - Animation test
+//
+//  Created by Сергей Ткаченко on 08.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
